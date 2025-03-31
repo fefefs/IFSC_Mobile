92e3ae261d38ea62262ac35d49b677f0e8a5a888
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2025-03-31 08:20:29 -0300

    Atividade IMC p.2
    Formatação de números e métodos.

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,16 +1,21 @@
 package com.ifsc.contaclick;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d("ciclo de vida", "metodo onCreate");
 
         setContentView(R.layout.activity_main);
         edPeso=findViewById((R.id.editTxtPeso));
@@ -36,8 +43,43 @@ public class MainActivity extends AppCompatActivity {
 
             imc = (peso/(altura * altura));
 
-            tvResultado.setText(Float.toString(imc));
+            //formatando número
+            DecimalFormat decimalFormat = new DecimalFormat("##.##");
+
+            tvResultado.setText(decimalFormat.format(imc));
         });
 
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ciclo de vida", "metodo onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ciclo de vida", "metodo onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ciclo de vida", "metodo onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("ciclo de vida", "metodo onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("ciclo de vida", "metodo onDestroy");
     }
 }
