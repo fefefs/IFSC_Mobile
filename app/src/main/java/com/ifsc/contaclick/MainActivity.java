eeeb7e460c1cd831f949a72c2dad668a1a3f3756
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2025-04-28 09:02:57 -0300

    Atividade IMC p.Final

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -2,39 +2,27 @@ package com.ifsc.contaclick;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    int i = 0;
-    EditText edPeso,edAltura;
+
+    EditText edPeso,edAltura, edNome;
     TextView tvResultado;
 
     TextView txtIMC;
     Button buttonCalcular;
 
-    Button buttonNextImage;
 
     ImageView img;
-
-    Integer imagens[] = new Integer[]{R.drawable.normal, R.drawable.obesidade1, R.drawable.obesidade2, R.drawable.obesidade3, R.drawable.perfil, R.drawable.sobrepeso, R.drawable.cachorro };
-
-    int imc = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,17 +34,41 @@ public class MainActivity extends AppCompatActivity {
         txtIMC=findViewById(R.id.txtCalcularIMC);
         edPeso=findViewById((R.id.editTxtPeso));
         edAltura=findViewById(R.id.editTxtAltura);
+        edNome=findViewById(R.id.editTextNome);
         buttonCalcular=findViewById(R.id.button);
         img=findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.perfil);
 
-        // tratamento click button
+
+
         buttonCalcular.setOnClickListener(v ->{
+            //calcular imc
             float peso,altura,imc;
             peso = Float.parseFloat(edPeso.getText().toString());
             altura = Float.parseFloat(edAltura.getText().toString());
 
             imc = (peso/(altura * altura));
 
+            //formatando número
+            DecimalFormat decimalFormat = new DecimalFormat("##.##");
+
+            //    tvResultado.setText(decimalFormat.format(imc));
+
+            Intent intent = new Intent(getApplicationContext(), MainActivityB.class);
+
+            String resultNome = edNome.getText().toString();
+            intent.putExtra("Nome",resultNome);
+
+            String resultAltura = edAltura.getText().toString();
+            intent.putExtra("Altura",resultAltura);
+
+            String resultPeso = edPeso.getText().toString();
+            intent.putExtra("Peso",resultPeso);
+
+            String resultIMC = String.valueOf(imc);
+            intent.putExtra("IMC",resultIMC);
+
+            startActivity(intent);
         });
 
     }
--- app/src/main/java/com/ifsc/contaclick/MainActivityB.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivityB.java
@@ -1,15 +1,29 @@
 package com.ifsc.contaclick;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+
 
 public class MainActivityB extends AppCompatActivity {
+
+    TextView IMCResult;
+    TextView nome;
+    TextView nomeResult;
+
+    TextView alturaResult;
+
+    TextView pesoResult;
+
+    ImageView imgIMC;
+
+    double imcCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +31,45 @@ public class MainActivityB extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_b);
         Bundle bundle = getIntent().getExtras();
-        String msg = bundle.getString("mensagem");
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(msg);
+
+        String result = bundle.getString("IMC");
+        IMCResult = findViewById(R.id.textView);
+        IMCResult.setText(result);
+
+        String nome = bundle.getString("Nome");
+        nomeResult = findViewById(R.id.txtNomeR);
+        nomeResult.setText(nome);
+
+        pesoResult=findViewById(R.id.txtPseoBR);
+        String peso = bundle.getString("Peso");
+        pesoResult.setText(peso);
+
+        String altura = bundle.getString("Altura");
+        alturaResult = findViewById(R.id.txtAlturaBR);
+        alturaResult.setText(altura);
+
+
+        imgIMC = findViewById(R.id.imgIMC);
+        imcCalc = Double.parseDouble(result);
+
+        if(imcCalc < 18.5) {
+            imgIMC.setImageResource(R.drawable.abaixopeso);
+        }
+        if(imcCalc >= 18.5 && imcCalc < 24.9) {
+            imgIMC.setImageResource(R.drawable.obesidade1);
+        }
+        if(imcCalc >= 25 && imcCalc < 29.9) {
+            imgIMC.setImageResource(R.drawable.sobrepeso);
+        }
+        if(imcCalc >= 30 && imcCalc < 34.9) {
+            imgIMC.setImageResource(R.drawable.obesidade1);
+        }
+        if(imcCalc >= 35 && imcCalc < 39.9) {
+            imgIMC.setImageResource(R.drawable.obesidade2);
+        }
+        if(imcCalc >= 40) {
+            imgIMC.setImageResource(R.drawable.obesidade3);
+        }
 
     }
 }
