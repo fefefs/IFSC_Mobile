c820b5f6c7d9b6b54f78f93e1e68aa0e674be61e
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2025-06-30 09:11:07 -0300

    Localizador

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,6 +1,12 @@
 package com.ifsc.contaclick;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
@@ -13,6 +19,7 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -21,20 +28,10 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    int i = 0;
-    EditText edPeso,edAltura;
-    TextView tvResultado;
+    LocationManager locationManager;
 
-    TextView txtIMC;
-    Button buttonCalcular;
-
-    Button buttonNextImage;
-
-    ImageView img;
-
-    Integer imagens[] = new Integer[]{R.drawable.normal, R.drawable.obesidade1, R.drawable.obesidade2, R.drawable.obesidade3, R.drawable.perfil, R.drawable.sobrepeso, R.drawable.cachorro };
-
-    int imc = 0;
+    TextView tvLatitude, tvLongitude, tvStatus;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,21 +40,56 @@ public class MainActivity extends AppCompatActivity {
         Log.d("ciclo de vida", "metodo onCreate");
 
         setContentView(R.layout.activity_main);
-        txtIMC=findViewById(R.id.txtCalcularIMC);
-        edPeso=findViewById((R.id.editTxtPeso));
-        edAltura=findViewById(R.id.editTxtAltura);
-        buttonCalcular=findViewById(R.id.button);
-        img=findViewById(R.id.imageView);
+        tvLatitude=findViewById(R.id.tvLatitude);
+        tvLongitude=findViewById(R.id.tvLongitude);
+        tvStatus=findViewById(R.id.tvStatus);
+        button=findViewById(R.id.button);
+        locationManager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
-        // tratamento click button
-        buttonCalcular.setOnClickListener(v ->{
-            float peso,altura,imc;
-            peso = Float.parseFloat(edPeso.getText().toString());
-            altura = Float.parseFloat(edAltura.getText().toString());
 
-            imc = (peso/(altura * altura));
 
+        button.setOnClickListener(v->{
+            getLocalizacao();
         });
+
+
+        public void getLocalizacao(){
+            if(checaESolicitaPermicao()){
+
+                tvLatitude.setText("Buscando localizacao");
+                @SuppressLint("MissingPermission") Location l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if(l!=null) {
+                    tvLatitude.setText(Double.toString(l.getLatitude()));
+                    tvLongitude.setText(Double.toString(l.getLongitude()));
+                } else {
+                    tvLatitude.setText("Sem localização");
+                }
+
+            } else {
+                tvLatitude.setText("Permição negada");
+            }
+
+        }
+
+        public boolean checaESolicitaPermicao(){
+            if(ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED ||
+               ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{ Manifest.permission.ACCESS_FINE_LOCATION,
+                                      Manifest.permission.ACCESS_COARSE_LOCATION
+                                      }, PackageManager.PERMISSION_GRANTED
+                        );
+                return false;
+            } else {
+                return true;
+            }
+        }
+
 
     }
 
